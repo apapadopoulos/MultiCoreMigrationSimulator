10035c06030dc6e2353afa4246a2c044f60628a4
apapadopoulos <alessandro.papadopoulos@gmail.com>
2015-02-17 17:56:31 +0100

    Changed execute experiments

--- execute.sh
+++ execute.sh
@@ -7,11 +7,11 @@ algs=( simple load_aware load_normalized )
 
 for algorithm in "${algs[@]}"
 do
-	for nT in 100 #250 500
+	for nT in 100 250 500
 	do
-		for rT in 0.1 #0.2 0.5 1.0
+		for rT in 0.1 0.2 0.5 1.0
 		do
-			for cc in 8 #16 32
+			for cc in 8 16 32
 			do
 			    mkdir -p results/${algorithm}
 			    ./mcms.py --migration ${algorithm} --outdir results/${algorithm}/ --simTime 1000 --numThreads ${nT} --numCores ${cc} --deltaSP 10 --relocationThreshold ${rT} $@ &
